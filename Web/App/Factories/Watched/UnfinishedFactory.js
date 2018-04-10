25ef2045755af2597ec6a7f2cdd2c3cf76de071f
JKorf <jankorf91@gmail.com>
2018-04-10 14:30:44 +0200

    Added caching unfinished torrents

--- Web/App/Factories/Watched/UnfinishedFactory.js
+++ Web/App/Factories/Watched/UnfinishedFactory.js
@@ -1,35 +1,27 @@
 (function () {
 
-    angular.module('pi-test').factory('UnfinishedFactory', function ($q, $http) {
+    angular.module('pi-test').factory('UnfinishedFactory', function ($q, $http, CacheFactory) {
         var factory = {};
         var unfinished;
 
         factory.GetUnfinished = function(){
-            var promise = $q.defer();
-            if(!unfinished){
-                $http.get("/database/get_unfinished_torrents").then(function(data){
-                    unfinished = [];
-                    for(var i = 0; i < data.data.length; i++){
-                        var obj = {
-                            name: data.data[i][1],
-                            url: data.data[i][2],
-                            image: data.data[i][3],
-                            position: data.data[i][4],
-                            length: data.data[i][5],
-                            watchedAt: new Date(0)
-                        };
-                        obj.watchedAt.setUTCSeconds(data.data[i][6] / 1000)
-                        unfinished.push(obj);
-                    }
-                    console.log(unfinished);
-                    promise.resolve(unfinished);
-                });
-            }else
-            {
-                promise.resolve(unfinished);
-            }
-
-            return promise.promise;
+            return CacheFactory.Get("/database/get_unfinished_torrents", 60).then(function (response) {
+                unfinished = [];
+                for(var i = 0; i < response.length; i++){
+                    var obj = {
+                        name: response[i][1],
+                        url: response[i][2],
+                        image: response[i][3],
+                        position: response[i][4],
+                        length: response[i][5],
+                        watchedAt: new Date(0)
+                    };
+                    obj.watchedAt.setUTCSeconds(response[i][6] / 1000)
+                    unfinished.push(obj);
+                }
+                console.log(unfinished);
+                return unfinished;
+            });
         }
 
         return factory;
