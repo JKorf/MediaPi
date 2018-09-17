85c6200f1ba4955601ee9ed88f44c01140836297
JKorf <jankorf91@gmail.com>
2018-09-17 12:53:22 +0200

    created dark theme

--- src/Interface/Mobile/App/Directives/Textbox/TextboxDirective.js
+++ src/Interface/Mobile/App/Directives/Textbox/TextboxDirective.js
@@ -9,13 +9,6 @@
             },
             templateUrl: '/App/Directives/Textbox/textbox.html',
             link: function ($scope, element, attrs) {
-                $scope.textboxFocus = function($event){
-                    $($event.target).css("border", "1px solid #3d7ed3");
-                }
-
-                $scope.textboxBlur = function($event){
-                     $($event.target).css("border", "1px solid #777");
-                }
             }
         };
     });
