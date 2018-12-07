f0c2f4a728dc65451882e7ab9ac21289b97c92a0
JKorf <jankorf91@gmail.com>
2018-12-07 16:15:18 +0100

    .

--- src/UI/WebNew/test.js
+++ src/UI/WebNew/test.js
@@ -1,18 +1,39 @@
-function Welcome(props){
-    return <div>Hi {props.name}</div>;
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>;
+  }
+  return <p>The water would not boil.</p>;
 }
 
-function App() {
-  return (
-    <div>
-      <Welcome name="Sara" />
-      <Welcome name="Cahal" />
-      <Welcome name="Edite" />
-    </div>
-  );
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {temperature: ''};
+  }
+
+  handleChange(e) {
+    this.setState({temperature: e.target.value});
+  }
+
+  render() {
+    const temperature = this.state.temperature;
+    return (
+      <fieldset>
+        <legend>Enter temperature in Celsius:</legend>
+        <input
+          value={temperature}
+          onChange={this.handleChange} />
+
+        <BoilingVerdict
+          celsius={parseFloat(temperature)} />
+
+      </fieldset>
+    );
+  }
 }
 
- ReactDOM.render(
-  <App />,
+ReactDOM.render(
+  <Calculator />,
   document.getElementById('root')
 );
