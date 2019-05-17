9d54aa4c9908efee221acb842007585bc8e61b75
Jan Korf <sevkorf@BD.com>
2019-05-17 16:04:46 +0200

    Added motor test

---
+++ motor/MotorController.py
@@ -0,0 +1,52 @@
+import RPi.GPIO as GPIO
+import time
+
+
+class MotorController:
+
+    steps_forward = [
+        [1, 0, 0, 0],
+        [1, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 1],
+        [0, 0, 0, 1],
+        [1, 0, 0, 1]
+    ]
+    steps_back = reversed(steps_forward)
+
+    def __init__(self, pins, ms_sleep, steps_per_revolution):
+        self.pins = pins
+        self.sleep = 0.001 * ms_sleep
+        self.steps_per_revolution = steps_per_revolution
+
+    def init(self):
+        GPIO.setmode(GPIO.BOARD)
+        for pin in self.pins:
+            GPIO.setup(pin, GPIO.OUT)
+            GPIO.output(pin, 0)
+
+    def cleanup(self):
+        GPIO.cleanup()
+
+    def move_steps(self, steps):
+        if steps > 0:
+            step_data = MotorController.steps_forward
+        else:
+            step_data = MotorController.steps_back
+
+        for i in range(steps):
+            for half_step in range(8):
+                self.__do_step(half_step, step_data)
+                time.sleep(self.sleep)
+
+    def move_angle(self, angle):
+        steps = self.steps_per_revolution / (360 / abs(angle))
+        if angle < 0:
+            steps = -steps
+        self.move_steps(steps)
+
+    def __do_step(self, half_step, step_data):
+        for pin in range(4):
+            GPIO.output(self.pins[pin], step_data[half_step][pin])
---
+++ motor/test.py
@@ -0,0 +1,15 @@
+from MotorController import MotorController
+
+
+revs = int(input("Revolutions: "))
+sleep = int(input("Milliseconds sleep: "))
+
+controller = MotorController([7, 11, 13, 15], sleep, 512)
+controller.init()
+
+print("Starting")
+for cycle in range(revs):
+    controller.move_angle(360)
+print("Done")
+
+controller.cleanup()
