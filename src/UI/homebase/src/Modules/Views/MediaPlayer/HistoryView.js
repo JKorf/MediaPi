2c6f819e91993734d1144fa3a5a9668f6ceacc16
Jan Korf <jankorf91@gmail.com>
2019-07-30 10:36:52 +0200

    fix history

--- src/UI/homebase/src/Modules/Views/MediaPlayer/HistoryView.js
+++ src/UI/homebase/src/Modules/Views/MediaPlayer/HistoryView.js
@@ -38,7 +38,9 @@ class HistoryView extends Component {
                             <div className="history-item-details">
                                 <div className="history-item-title truncate2">{history.title}</div>
                                 <div className="history-item-type">{history.type}</div>
-                                <div className="history-item-time">{new Intl.DateTimeFormat('en-GB', { year: 'numeric', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit' }).format(new Date(1970, 0, 0).setTime(history.watched_at))}</div>
+                                { history.watched_at &&
+                                    <div className="history-item-time">{new Intl.DateTimeFormat('en-GB', { year: 'numeric', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit' }).format(new Date(1970, 0, 0).setTime(history.watched_at))}</div>
+                                }
                             </div>
                         </div>)}) }
                     </div>
