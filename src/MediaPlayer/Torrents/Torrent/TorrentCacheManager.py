5aec5c5eb8cbe19159228fe648416d2a21a98b60
Jan Korf <jankorf91@gmail.com>
2019-08-09 13:15:39 +0200

    Refactored cache

--- src/MediaPlayer/Torrents/Torrent/TorrentCacheManager.py
+++ src/MediaPlayer/Torrents/Torrent/TorrentCacheManager.py
@@ -7,53 +7,56 @@ class TorrentCacheManager(TorrentManager):
 
     def __init__(self, torrent):
         super().__init__(torrent, "cache")
-        self.cache_file = None
+        self.open_cache_files = []
         self.piece_length = 0
         self.total_length = 0
         self.media_start_byte = 0
         self.media_start_piece_offset = 0
-        self.path = os.getcwd() + '\\Solution\\cache.tmp'
+        self.path = os.getcwd() + '\\Solution\\cache\\'
 
         self.bytes_written = 0
 
     def init(self, piece_length, total_length, media_start_byte):
-        self.remove_cache_file()
+        if not os.path.exists(self.path):
+            os.mkdir(self.path)
 
-        # Have to open and close it to create the file and prevent not found exception when also trying to read
-        self.cache_file = open(self.path, "a+b")
-        self.cache_file.close()
+        self.remove_cache_files()
 
-        self.cache_file = open(self.path, "r+b")
-        self.cache_file.truncate(total_length)
         self.piece_length = piece_length
         self.total_length = total_length
         self.media_start_byte = media_start_byte
         self.media_start_piece_offset = self.media_start_byte % self.piece_length
 
     def read_bytes(self, start_byte, length):
-        self.cache_file.seek(start_byte, 0)
-        return self.cache_file.read(length)
+        actual_start_byte = start_byte + self.media_start_byte
+        pieces = self.torrent.data_manager.get_all_pieces_in_range(actual_start_byte, actual_start_byte + length)
+
+        offset = actual_start_byte - pieces[0].start_byte
+        result = bytearray()
+        for piece in pieces:
+            with open(self.path + str(piece.index), "rb") as data_file:
+                if offset is not 0:
+                    data_file.seek(offset, 0)
+                result += data_file.read(self.piece_length - offset)
+            offset = 0
+        return result[0: length]
 
     def write_piece(self, piece):
-        start_byte = piece.start_byte - self.media_start_byte
-        data = piece.get_data()
-        if piece.start_byte < self.media_start_byte:
-            data = data[self.media_start_piece_offset:]
-            start_byte = 0
-
-        self.cache_file.seek(start_byte, 0)
-        self.cache_file.write(data)
-        self.cache_file.flush()
+        with open(self.path + str(piece.index), "ab") as data_file:
+            data_file.write(piece.get_data())
         piece.written = True
-        self.bytes_written += len(data)
         piece.clear()
+        self.bytes_written += piece.length
 
-    def remove_cache_file(self):
-        if os.path.exists(self.path):
-            os.remove(self.path)
+    def remove_cache_files(self):
+        for the_file in os.listdir(self.path):
+            file_path = os.path.join(self.path, the_file)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+            except Exception as e:
+                print(e)
 
     def stop(self):
-        if self.cache_file:
-            self.cache_file.close()
-        self.remove_cache_file()
+        self.remove_cache_files()
         super().stop()
